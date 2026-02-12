cf7706103442e19285b72191eb713e35a9e8fe42
ys6573-dot <Ys6573@srmist.edu.in>
2026-02-12 22:25:32 +0530

    completed UC1: print OOPS to console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,6 @@
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        // This is Use Case 1: Printing OOPS to the console
+        System.out.println("OOPS");
+    }
+}
